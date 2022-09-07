921d0dfbf397f856c331ed314ff2bdab2df013a0
Elias Matheus Oliveira <elias.matheus@rainforest.tech>
2022-09-07 14:43:33 -0300

    Add Navigation with Event Binding and ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  route = 'recipes';
+
+  @Output() routeChanged = new EventEmitter<string>();
+
+  changeRoute(route: string) {
+    this.route = route;
+
+    this.routeChanged.emit(this.route);
+  }
+}
